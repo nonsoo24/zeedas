7f3d3c7b4b1d34210126cdba06bc9ab856fcf949
Daniel Chukwurah <daniel.chukwurah@gmail.com>
2023-04-21 20:02:19 +0100

    feat: implement resume screen

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,5 +1,55 @@
-import React from "react";
+import ArrowUpIcon from "assets/svg/ArrowUpIcon";
+import InfoIcon from "assets/svg/InfoIcon";
+import CardStats from "components/shared-components/Card/CardStats";
+import HeaderTitle from "components/shared-components/HeaderTitle";
+import UserProfile from "components/shared-components/UserProfile.js";
+import React, { useMemo } from "react";
+import "./Resume.css";
 
-const Resume = () => <div className="wrapper container">Resume</div>;
+const Resume = () => {
+  const stats = useMemo(
+    () => [
+      {
+        append: (
+          <div className="d-flex align-items-center">
+            <h6 className="resume-card-title">Speed</h6>
+            <InfoIcon />
+          </div>
+        ),
+        count: "2.8c/hr",
+        percentageChange: "87%",
+        icon: <ArrowUpIcon />,
+        chartColor: "#429777",
+        variant: "emerald",
+        tagClass: "profile-visit speed",
+      },
+      {
+        append: (
+          <div className="d-flex align-items-center">
+            <h6 className="resume-card-title">Accuracy</h6>
+            <InfoIcon />
+          </div>
+        ),
+        count: "89%",
+        percentageChange: "79%",
+        icon: <ArrowUpIcon />,
+        chartColor: "#429777",
+        variant: "emerald",
+        tagClass: "profile-visit accuracy",
+      },
+    ],
+    [],
+  );
+  return (
+    <div className="resume">
+      <UserProfile showRole />
+
+      <div className="resume-metrics">
+        <HeaderTitle title="Metrics" titleClass="metrics-title" />
+        <CardStats items={stats} cardStatWrapperStyle="cardStat" />
+      </div>
+    </div>
+  );
+};
 
 export default Resume;
--- src/pages/ViewerStats/index.js
+++ src/pages/ViewerStats/index.js
@@ -1,5 +1,55 @@
-import React from "react";
+import ArrowDown from "assets/svg/ArrowDownIcon";
+import ArrowUpIcon from "assets/svg/ArrowUpIcon";
+import Card from "components/shared-components/Card";
+import CardStats from "components/shared-components/Card/CardStats";
+import HeaderTitle from "components/shared-components/HeaderTitle";
+import Table from "components/shared-components/Table";
+import React, { useMemo } from "react";
+import tableData from "utils/mock-data/table";
+import "./ViewerStats.css";
 
-const ViewerStats = () => <div className="wrapper container">ViewerStats</div>;
+const ViewerStats = () => {
+  const stats = useMemo(
+    () => [
+      {
+        title: "Profile visits",
+        count: 732,
+        percentageChange: "87%",
+        icon: <ArrowUpIcon />,
+        chartColor: "#429777",
+        variant: "emerald",
+        tagClass: "profile-visit",
+      },
+      {
+        title: "Time spent (hrs)",
+        count: 328,
+        percentageChange: "27%",
+        icon: <ArrowDown />,
+        chartColor: "#F15832",
+        variant: "red",
+        tagClass: "time-spent",
+      },
+    ],
+    [],
+  );
+
+  return (
+    <div className="wrapper container">
+      <HeaderTitle
+        title="Profile analytics"
+        showDropdown
+        items={[{ label: "Last 30 days", value: "all" }]}
+      />
+      <CardStats items={stats} />
+
+      <div>
+        <Card cardClass="recent-visitors">
+          <HeaderTitle title="Recent visitors" titleClass="visitor-header" />
+          <Table data={tableData} />
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default ViewerStats;
